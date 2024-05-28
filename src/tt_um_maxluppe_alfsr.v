0b7630d3f4ff58190b7d2b48938bb5a89727058e
Maximiliam Luppe <maxluppe@users.noreply.github.com>
2024-05-28 20:57:03 -0300

    Update tt_um_maxluppe_alfsr.v
    Included NIST tests

--- src/tt_um_maxluppe_alfsr.v
+++ src/tt_um_maxluppe_alfsr.v
@@ -16,20 +16,35 @@ module tt_um_maxluppe_alfsr (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+    reg RND_in;
+    wire RND_out;
+    
     // All output pins must be assigned. If not used, assign to 0.
-    assign uio_out = 0;
-    assign uio_oe  = 0;
+    assign uio_out[7:2] = 0;
+    assign uio_oe  = 8'b00000011;
 
     alfsr alfsr0 (.clk(clk),				//Digitalization clock
                   .rst_n(rst_n),			//LFSR Configurator reset
                   .lfsr_clk(ui_in[0]),		//LFSR Configurator clock
                   .alfsr_rst_n(ui_in[1]),	//ALFSR reset
-                  .lfsr_out(uo_out[6]),		//LFSR Configuratior output
-                  .rng_out_d(uo_out[5]),	//ALFSR 'digitalized' output
+                  .lfsr_out(uo_out[5]),		//LFSR Configuratior output
+                  .rng_out_d(RND_out),	//ALFSR 'digitalized' output
                   .rng_out(uo_out[3:0])	//ALFSR 'analog' outputs
     );
 
-    assign uo_out[4] = 0;
-    assign uo_out[7] = 0;
+    assign uo_out[4] = RND_out;
+    
+    always @(negedge(clk)) begin
+        RND_in <= uo_out[4];
+    end
+    
+    NIST_SP_800_22 NIST0123 (.clk(clk),
+                             .rstn(rst_n),
+                             .RND_in(RND_in),
+                             .error1(uo_out[6]),
+                             .error2(uo_out[7]),
+                             .error3(uio_out[0]),
+                             .error4(uio_out[1])
+    );
 
 endmodule
